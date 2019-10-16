27b66ee96efa7efb4f71a3e4f1a10dc40776e35d
rajasekhartmk <48428721+rajasekhartmk@users.noreply.github.com>
2019-10-16 17:37:35 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,15 +1,7 @@
 package day7_session1;
-class Aa{
-	public static void f(){
-		System.out.println("A");
-	}
-}
+
 class Demo extends Aa {
-	public static void f(){
-		System.out.println("B");
-	}
 	public static void main(String[] args) {
-		Aa a=new Aa();
-		a.f();
+		System.out.println("hello world");
 	}
 }
